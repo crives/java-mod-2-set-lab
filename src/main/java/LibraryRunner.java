c4f65013c980a9a15a9071fd65e5e39d1e1f6633
Caroline Rives <car.rives@gmail.com>
2022-06-29 14:49:21 -0400

    Optimized code

--- src/main/java/LibraryRunner.java
+++ src/main/java/LibraryRunner.java
@@ -11,10 +11,18 @@ import java.util.Scanner;
 public class LibraryRunner {
 
     public static Scanner sc = new Scanner(System.in);
+    public static boolean isGenrePresent = false;
     public static boolean repeat = true;
     public static String addBookMsg = "\n-- Would you like to: \n 1. Add a book \n 2. Look up an existing book \n 3. Search a book by genre \n 4. Print all books \n 5. Exit \n \nType the number of the option you choose from above:";
     public static String addBookMsgAgain = "\n-- Would you like to: \n 1. Add another book \n 2. Look up an existing book \n 3. Search a book by genre \n 4. View all books \n 5. Exit \n \nType the number of the option you choose from above:";
-    
+    public static String askTitle = "\nWhat is the title of the book you are searching?";
+    public static String askGenre = "\nWhat is the genre of the book you are searching?";
+    public static String foundBookByTitle = "\nThis is the book you are looking for:\n";
+    public static String didNotFindByTitle = "\n*** \nSorry! We don't have that book in our records.... \n***";
+    public static String noBooks = "\n*** \nThe library is empty at this time....no records! \n***";
+    public static String containsGenre = "\nYes, we do have a book of that genre!\nSee below: \n";
+    public static String noGenre = "\nSorry, we do not currently have a book of that genre!";
+
     public static void main(String[] args) {
         System.out.println("\nWelcome to the Virtual Book Library, where you can create and look up books!");
         intro();
@@ -33,11 +41,17 @@ public class LibraryRunner {
                     break;
                 case 2:
                     String str1 = sc.nextLine();
-                    lookUpBook();
+                    System.out.println(askTitle);
+                    String title = sc.nextLine();
+                    lookUpTitle(title);
+                    intro();
                     break;
                 case 3:
                     String str2 = sc.nextLine();
-                    searchGenre();
+                    System.out.println(askGenre);
+                    String genre = sc.nextLine().toLowerCase();
+                    lookUpGenre(genre);
+                    intro();
                     break;
                 case 4:
                     Library.printBooks();
@@ -70,33 +84,24 @@ public class LibraryRunner {
         System.out.println("\n***** Book has been added! *****");
     }
 
-    public static void lookUpBook() {
-        System.out.println("\nWhat is the title of the book you are searching?");
-        String title = sc.nextLine();
+    public static void lookUpTitle(String title) {
+        String message = Library.bookMap.containsKey(title) 
+            ? foundBookByTitle + Library.bookMap.get(title)
+            : didNotFindByTitle;
+        System.out.println(message);
+   }
 
-        if (Library.bookMap.containsKey(title)) {
-            System.out.println("\nThis is the book you are looking for:\n" + Library.bookMap.get(title));
-            intro();
-        } else {
-            System.out.println("\n*** \nSorry! We don't have that book in our records.... \n***");
-            intro();
-        }
-    }
+    public static void lookUpGenre(String genre) {
+        boolean isGenrePresent = Library.uniqueGenres.contains(genre);
+        String message1 = Library.bookMap.isEmpty()
+            ? noBooks 
+            : isGenrePresent
+                ? containsGenre
+                : noGenre;
+        System.out.println(message1);
 
-    public static void searchGenre() {
-        System.out.println("\nWhat is the genre of the book you are searching?");
-        String genre = sc.nextLine().toLowerCase();
-
-        if (Library.bookMap.isEmpty()) {
-            System.out.println("\n*** \nThe library is empty at this time....no records! \n***");
-            intro();
-        } else if(Library.uniqueGenres.contains(genre)) {
-            System.out.println("\nYes, we do have a book of that genre!\nSee below: \n");
+        if(isGenrePresent) {
             Library.printBooksWGenres(genre);
-            intro();
-        } else {
-            System.out.println("\nSorry, we do not currently have a book of that genre!");
-            intro();
         }
-    }
+    }  
 }
